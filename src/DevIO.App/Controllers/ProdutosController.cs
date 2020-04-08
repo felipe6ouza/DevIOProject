cda7cc2cbd4d5646a0a85aba4b32c3341af5c4c7
Felipe Souza <felipe.souza@dcomp.ufs.br>
2020-04-08 20:30:34 -0300

    Customizar Views e upload de imagem de produto

--- src/DevIO.App/Controllers/ProdutosController.cs
+++ src/DevIO.App/Controllers/ProdutosController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using DevIO.App.ViewModels;
 using DevIO.Business.Interfaces;
 using DevIO.Business.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace DevIO.App.Controllers
 {
@@ -51,17 +53,27 @@ namespace DevIO.App.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(ProdutoViewModel produtoViewModel)
         {
-             produtoViewModel = await PopularFornecedores(new ProdutoViewModel());
+             produtoViewModel = await PopularFornecedores(produtoViewModel);
 
 
             if (!ModelState.IsValid) return View(produtoViewModel);
 
+            var imgPrefixo = Guid.NewGuid() + "_";
+
+            if (!await UploadArquivo(produtoViewModel.ImagemUpload, imgPrefixo))
+            {
+                return View(produtoViewModel);
+            }
+
+            produtoViewModel.Imagem = imgPrefixo + produtoViewModel.ImagemUpload.FileName;
+
             await _produtoRepository.Adicionar( _mapper.Map<Produto>(produtoViewModel));
 
-            return View(produtoViewModel);
+            return RedirectToAction("Index");
 
         }
 
+        
         public async Task<IActionResult> Edit(Guid id)
         {
 
@@ -122,6 +134,31 @@ namespace DevIO.App.Controllers
             produto.Fornecedores = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
             return produto;
         }
- 
+
+        private async Task<bool> UploadArquivo(IFormFile arquivo, string imgPrefixo)
+        {
+
+            if (arquivo.Length <= 0)
+            {
+                return false;
+            }
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", imgPrefixo + arquivo.FileName);
+
+            if (System.IO.File.Exists(path))
+            {
+                ModelState.AddModelError(String.Empty, "Já existe um arquivo com este nome");
+                return false;
+            }
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await arquivo.CopyToAsync(stream);
+            }
+
+            return true;
+
+        }
+
     }
 }
--- src/DevIO.App/ViewModels/ProdutoViewModel.cs
+++ src/DevIO.App/ViewModels/ProdutoViewModel.cs
@@ -13,21 +13,22 @@ namespace DevIO.App.ViewModels
     {
         [Key]
         public Guid Id { get; set; }
-
+        
+        [DisplayName("Fornecedor")]
         [Required(ErrorMessage = "O campo é {0} obrigatório")]
-        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {1} e {2} caracteres", MinimumLength = 2)]
         public Guid FornecedorId { get; set; }
 
 
         [Required(ErrorMessage = "O campo é {0} obrigatório")]
-        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {1} e {2} caracteres", MinimumLength = 2)]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Nome { get; set; }
 
         [DisplayName("Descrição")]
         [Required(ErrorMessage = "O campo é {0} obrigatório")]
-        [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {1} e {2} caracteres", MinimumLength = 2)]
+        [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Descricao { get; set; }
-
+        
+        [DisplayName("Upload de Imagem")]
         public IFormFile ImagemUpload { get; set; }
         public string Imagem { get; set; }
 
